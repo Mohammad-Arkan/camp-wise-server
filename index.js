0176136b18d4fc892fa242029d9e050d3c32dadd
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-06-10 15:29:59 +0600

    selected class get api

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+//jwt verify middleware
+const verifyJWT = (req, res, next)=>{
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: "unouthorized access"});
+    }
+
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
+        if(err){
+            res.status(401).send({error: true, message: 'unouthorized access'})
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
 
 
 
