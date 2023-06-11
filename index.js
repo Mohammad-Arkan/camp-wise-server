e91229d78d0c6d41790d2b5e25415151c1f3b7be
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-06-11 19:36:12 +0600

    add class

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ app.use(cors())
 app.use(express.json())
 
 //jwt verify middleware
-const verifyJWT = (req, res, next)=>{
+const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
-    if(!authorization){
-        return res.status(401).send({error: true, message: "unouthorized access"});
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "unouthorized access" });
     }
 
     const token = authorization.split(' ')[1];
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
-        if(err){
-            res.status(401).send({error: true, message: 'unouthorized access'})
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            res.status(401).send({ error: true, message: 'unouthorized access' })
         }
         req.decoded = decoded;
         next()
@@ -50,22 +50,64 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        //collections
         const instructorCollection = client.db('Summer-Camp').collection('instructor');
+
+        const userCollection = client.db('Summer-Camp').collection('user');
+
         const popularClassCollection = client.db('Summer-Camp').collection('Popularclass');
+
         const classesCollection = client.db('Summer-Camp').collection('classes');
+
         const selectedClassCollection = client.db('Summer-Camp').collection('SelectedClasses');
+
         const paymentCollection = client.db('Summer-Camp').collection('payments');
 
 
 
         //JWT
-        app.post('/jwt', (req, res)=>{
+        app.post('/jwt', (req, res) => {
             const email = req.body;
-            const token = jwt.sign(email, process.env.JWT_SECRET, {expiresIn: '1h'})
-            res.send({token})
-            
+            const token = jwt.sign(email, process.env.JWT_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+
         })
 
+
+        //verify instructor
+        const verifyInsructor = async(req, res, next)=>{
+            const email = req.decoded.email;
+            const query = {email: email};
+            const user = await userCollection.findOne(query);
+            if(user.role !== 'instructor'){
+                return res.status(403).send({error: true, message: 'forbidden access'})
+            }
+            next()
+        }
+
+
+        //save user 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const query = { email: email };
+            const option = { upsert: true };
+            const updateDoc = { $set: user }
+
+            const result = await userCollection.updateOne(query, updateDoc, option);
+            res.send(result)
+        })
+
+
+        //get instructor
+        app.get('/user/instructor/:email', verifyJWT, verifyInsructor, async(req, res)=>{
+            const email = req.params.email;
+            const query = {email: email};
+            const user = await userCollection.findOne(query);
+
+            const result ={instructor: user?.role === 'instructor'}
+            res.send(result)
+        })
 
         // get all instructor
         app.get('/instructor', async (req, res) => {
@@ -86,28 +128,34 @@ async function run() {
             res.send(result)
         })
 
+        app.post('/add-class',verifyJWT, verifyInsructor, async(req, res)=>{
+            const classData = req.body;
+            const result = await classesCollection.insertOne(classData);
+            res.send(result)
+        })
+
 
         //get selected class
-        
-        app.get('/selected-class', async(req, res)=>{
+
+        app.get('/selected-class', async (req, res) => {
             const email = req.query.email;
-            const query = {studentEmail: email}
+            const query = { studentEmail: email }
             const result = await selectedClassCollection.find(query).toArray();
             res.send(result)
         })
 
         //get payment class
-        app.get('/payment/:id',verifyJWT, async(req, res)=>{
+        app.get('/payment/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await selectedClassCollection.findOne(query);
             res.send(result)
         })
 
         //create-payment-intent
-        app.post('/create-payment-intent',verifyJWT, async(req, res)=>{
-            const {price} = req.body;
-            const amount = price*100;
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const { price } = req.body;
+            const amount = price * 100;
 
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
@@ -121,25 +169,29 @@ async function run() {
         })
 
         //save payment data
-        app.post('/payments',verifyJWT, async(req, res)=>{
+        app.post('/payments', verifyJWT, async (req, res) => {
             const payment = req.body;
             const insertResult = await paymentCollection.insertOne(payment);
 
-            const query = {_id: new ObjectId(payment.selectedClassId)};
+            const query = { _id: new ObjectId(payment.selectedClassId) };
             const deleteResult = await selectedClassCollection.deleteOne(query);
 
-            const querySeats = {_id: new ObjectId(payment.classesId)};
-            const option = {$inc: {availableSeats: -1, students: 1}};
+            const querySeats = { _id: new ObjectId(payment.classesId) };
+            const option = { $inc: { availableSeats: -1, students: 1 } };
             const availableSeatsResult = await classesCollection.updateOne(querySeats, option);
 
-            res.send({insertResult, deleteResult, availableSeatsResult })
+            res.send({ insertResult, deleteResult, availableSeatsResult })
         })
 
+
+
+
+
         //get my classes data form payment database
-        app.get('/my-classes',verifyJWT, async(req, res)=>{
+        app.get('/my-classes', verifyJWT, async (req, res) => {
             const email = req.query.email;
-            const query = {email: email};
-            const result = await paymentCollection.find(query).sort({date: -1}).toArray();
+            const query = { email: email };
+            const result = await paymentCollection.find(query).sort({ date: -1 }).toArray();
             res.send(result)
         })
 
@@ -153,9 +205,9 @@ async function run() {
 
         //DELETE
         //delete selected class
-        app.delete('/selected-classes/:id',verifyJWT, async(req, res)=>{
+        app.delete('/selected-classes/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await selectedClassCollection.deleteOne(query);
             res.send(result)
         })
