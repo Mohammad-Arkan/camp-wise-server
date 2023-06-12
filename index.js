18200d363c03e3360c1cc9e19f5b020cbf219536
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-06-12 20:15:16 +0600

    instructor get

--- index.js
+++ index.js
@@ -48,10 +48,9 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         //collections
-        const instructorCollection = client.db('Summer-Camp').collection('instructor');
 
         const userCollection = client.db('Summer-Camp').collection('user');
 
@@ -143,8 +142,12 @@ async function run() {
 
         // get all instructor
         app.get('/instructor', async (req, res) => {
+            const result = await classesCollection.find().sort({ students: -1 }).toArray();
+            res.send(result)
+        })
 
-            const result = await instructorCollection.find().sort({ students: -1 }).limit(6).toArray()
+        app.get('/home/instructor', async(req, res)=>{
+            const result = await classesCollection.find().sort({ students: -1 }).limit(6).toArray();
             res.send(result)
         })
 
@@ -327,7 +330,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
